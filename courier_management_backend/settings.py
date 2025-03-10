8374aab3acb861b6f0adf5ad726cf996c3db9269
Nitay_Das <nitaydas910@gmail.com>
2025-03-11 00:29:41 +0600

    changes database

--- courier_management_backend/settings.py
+++ courier_management_backend/settings.py
@@ -23,17 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=!5(s5i&&_=_e^)bycq*8!izzy3hitl&_c(=@1zb$)7_ivtl+y'
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-=!5(s5i&&_=_e^)bycq*8!izzy3hitl&_c(=@1zb$)7_ivtl+y' 
+SECRET_KEY = os.environ.get("SECRET_KEY")  #for development
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-# ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0' 'courier-management-backend-1.onrender.com']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ") #for development
+# ALLOWED_HOSTS = []
 
-# DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'  #for development
 
-DEBUG = True
+# DEBUG = True
 
 
 
@@ -93,10 +93,9 @@ DATABASES = {
     }
 }
 
-# postgresql://courier_system_render_user:rLgXWvRfKkOiF1XqrnZRMd5vBmCLZlQd@dpg-cv7h92tsvqrc739p0980-a.oregon-postgres.render.com/courier_system_render
 
-# database_url = os.environ.get("DATABASE_URL")
-# DATABASES['default'] = dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 
 # Password validation
